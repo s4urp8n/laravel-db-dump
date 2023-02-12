bf1e10541e300e387141fe453e2ae8a61203b917
tarasov <s4urp8n@yandex.ru>
2023-02-12 12:27:46 +0300

    mysql dump driver realization

--- src/Command/Dump/Driver/Base.php
+++ src/Command/Dump/Driver/Base.php
@@ -4,5 +4,5 @@ namespace s4urp8n\DatabaseDump\Command\Dump\Driver;
 
 abstract class Base
 {
-    public abstract function dump(string $connection, string $database, string $table, string $savepath);
+    public abstract function dump(string &$connection, string &$database, string &$table, string &$savepath);
 }
--- src/Command/Dump/Driver/MySql.php
+++ src/Command/Dump/Driver/MySql.php
@@ -1,68 +1,49 @@
 <?php
 
 namespace s4urp8n\DatabaseDump\Command\Dump\Driver;
+
+use Illuminate\Support\Facades\DB;
+
 class MySql extends Base
 {
-    public function detectMysqlBinary($file)
+    public function detectMysqlBinary(&$connection, &$file)
     {
-//        $dirErrorMessage = 'Cant detect mysql binary ' . $file;
-//
-//        $basedir = Sql::select('show variables where variable_name=\'basedir\'');
-//        if (!is_array($basedir) || !$basedir) {
-//            throw new \Exception($dirErrorMessage);
-//        }
-//
-//        $basedir = $basedir[0] ?? false;
-//        if (!is_array($basedir) || !$basedir) {
-//            throw new \Exception($dirErrorMessage);
-//        }
-//
-//        $basedir = array_values($basedir);
-//        $basedir = $basedir[1] ?? false;
-//        if (!$basedir || !is_dir($basedir)) {
-//            throw new \Exception($dirErrorMessage);
-//        }
-//
-//        $binary = $basedir . DIRECTORY_SEPARATOR . 'bin' . DIRECTORY_SEPARATOR . $file;
-//        if (DIRECTORY_SEPARATOR == '\\') {
-//            $binary .= '.exe';
-//        }
-//
-//        if (!file_exists($binary)) {
-//            throw new \Exception($dirErrorMessage);
-//        }
-//
-//        return $binary;
+        $basedir = (array)DB::connection($connection)->selectOne('show variables where variable_name=\'basedir\'');
+        $basedir = array_values($basedir)[1] ?? null;
+
+        if (!$basedir || !is_dir($basedir)) {
+            throw new \Exception('Cant detect mysql binary ' . $file);
+        }
+
+        $binary = $basedir . DIRECTORY_SEPARATOR . 'bin' . DIRECTORY_SEPARATOR . $file;
+        if (DIRECTORY_SEPARATOR == '\\') {
+            $binary .= '.exe';
+        }
+
+        if (!file_exists($binary)) {
+            throw new \Exception('Mysql binary ' . $file . ' is not exists');
+        }
+
+        return $binary;
     }
 
-    public function dump(string $connection, string $database, string $table, string $savepath)
+    public function dump(string &$connection, string &$database, string &$table, string &$savepath)
     {
-//        $database = env('DB_DATABASE');
-//        $username = env('DB_USERNAME');
-//        $password = env('DB_PASSWORD');
-//        $mysqldump = $this->detectMysqlBinary('mysqldump');
-//        $date = date('Y-m-d H_i_s');
-//        $dir = base_path('dumps' . DIRECTORY_SEPARATOR . $date);
-//        $tables = Sql::select('show tables', 0);
-//        mkdir($dir, 0777, true);
-//        putenv('MYSQL_PWD=' . $password);
-//
-//        foreach ($tables as $table) {
-//            if (preg_match('/^memory_/i', $table)) {
-//                continue;
-//            }
-//
-//            echo "\n\n", $table, "...\n";
-//
-//            $output = $dir . DIRECTORY_SEPARATOR . $table . '.sql';
-//            $cmd = sprintf('%s --add-drop-table --add-locks --disable-keys --extended-insert --insert-ignore --quote-names --routines --verbose --triggers --events --force -u%s %s %s > %s',
-//                escapeshellarg($mysqldump),
-//                escapeshellarg($username),
-//                escapeshellarg($database),
-//                escapeshellarg($table),
-//                escapeshellarg($output)
-//            );
-//
-//            passthru($cmd);
+        $mysqldump = $this->detectMysqlBinary($connection, 'mysqldump');
+
+        $username = config('database.connections.' . $connection . '.username');
+        $password = config('database.connections.' . $connection . '.password');
+
+        putenv('MYSQL_PWD=' . $password);
+
+        $cmd = sprintf('%s --add-drop-table --add-locks --disable-keys --extended-insert --insert-ignore --quote-names --routines --verbose --triggers --events --force -u%s %s %s > %s',
+            escapeshellarg($mysqldump),
+            escapeshellarg($username),
+            escapeshellarg($database),
+            escapeshellarg($table),
+            escapeshellarg($savepath)
+        );
+
+        passthru($cmd);
     }
 }
